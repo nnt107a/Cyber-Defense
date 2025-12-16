b866b47a8b19684204425773588aedd432d8a5ca
oahtueid <thao452004@gmail.com>
2025-12-16 22:05:47 +0700

    feat: add Vorrath and simple spawn enemy script

--- Assets/Scripts/Unit/Enemies/Enemy.cs
+++ Assets/Scripts/Unit/Enemies/Enemy.cs
@@ -28,7 +28,11 @@ public class Enemy : MonoBehaviour
         //     return;
         // }
         attackTimer += Time.deltaTime;
-        if (attackTimer >= attackInterval && enemiesInLane)
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, Vector2.left, 0.5f, 0);
+        Debug.Log("Enemy Raycast Hit: " + hit.collider);
+        Debug.DrawLine(rb.position, hit.point, Color.red);
+
+        if (attackTimer >= attackInterval && hit.collider?.GetComponent<Turret>())
         {
             Attack();
             attackTimer = -10f;
