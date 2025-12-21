975d02aaea578263dd04a42dbdd87b23a8881ba1
vow130504 <22120344@student.hcmus.edu.vn>
2025-12-21 20:02:31 +0700

    complete 2 enemy

---
+++ Assets/Scripts/Unit/Enemies/Emberwolf.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Emberwolf : Enemy
+{
+    
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+    }
+    
+}
---
+++ Assets/Scripts/Unit/Enemies/Windrazor.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Windrazor : Enemy
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+    }
+}
