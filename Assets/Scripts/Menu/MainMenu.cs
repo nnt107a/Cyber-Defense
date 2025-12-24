0b5749299351cbc9c782fb25554847e6a6b35ee5
vow130504 <22120344@student.hcmus.edu.vn>
2025-12-25 01:59:35 +0700

    Play Game

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+
+        
+    }
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        
+    }
+}
